69551e42692616bdef835a5e5ad805332fffb6ae
Alwyn <idan.4tal@gmail.com>
2023-11-15 22:22:17 +0200

    UpdateCode

--- app/[[...category]]/CategoriesList.tsx
+++ app/[[...category]]/CategoriesList.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import Link from "next/link";
-import { useContext, useEffect, useRef } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
+import { FaCircleChevronLeft, FaCircleChevronRight } from "react-icons/fa6";
 
 import { MenuContext, SearchContext } from "@/app/components/RootApp";
 
@@ -22,8 +23,13 @@ export function CategoriesList({
 	categoryId?: string;
 }) {
 	const ref = useRef<HTMLDivElement>(null);
+	const navbarRef = useRef<HTMLDivElement>(null);
+
+	const [showLeftArrow, setShowLeftArrow] = useState(false);
+	const [showRightArrow, setShowRightArrow] = useState(true);
 	const { search, onSearch } = useContext(SearchContext);
 	// const { isMobile, isMenuOpen, toggleMenu } = useContext(MenuContext);
+
 	useEffect(() => {
 		const selected = document.querySelector(".links-section.desktop-open");
 		if (!selected) return;
@@ -33,9 +39,55 @@ export function CategoriesList({
 		});
 	}, []);
 
+	useEffect(() => {
+		const container = navbarRef.current;
+		if (container) {
+			const checkScrollArrows = () => {
+				const isAtStart = container.scrollLeft === 0;
+				const isAtEnd =
+					container.scrollLeft + container.offsetWidth ===
+					container.scrollWidth;
+				setShowLeftArrow(!isAtStart);
+				setShowRightArrow(!isAtEnd);
+			};
+
+			// Initial check
+			checkScrollArrows();
+
+			// Set up event listener for scroll
+			container.addEventListener("scroll", checkScrollArrows, {
+				passive: true,
+			});
+
+			// Clean up event listener on unmount
+			return () => container.removeEventListener("scroll", checkScrollArrows);
+		}
+	}, []);
+
+	function scrollLeft() {
+		const container = document.querySelector(".icon-navbar");
+		if (container) {
+			container.scrollBy({ left: -300, behavior: "smooth" });
+		}
+	}
+
+	function scrollRight() {
+		const container = document.querySelector(".icon-navbar");
+		if (container) {
+			container.scrollBy({ left: 300, behavior: "smooth" });
+		}
+	}
+
 	return (
 		<div className={`desktop-category`}>
-			<div dir="rtl" className="icon-navbar">
+			<div dir="rtl" className="icon-navbar" ref={navbarRef}>
+				<div
+					className={`scroll-arrow left ${showLeftArrow ? "" : "hidden"}`}
+					onClick={scrollLeft}
+				>
+					<FaCircleChevronLeft />
+				</div>
+
 				{categories.map((category) => (
 					<div
 						ref={ref}
@@ -75,6 +127,13 @@ export function CategoriesList({
 						</Link>
 					</div>
 				))}
+
+				<div
+					className={`scroll-arrow right ${showRightArrow ? "" : "hidden"}`}
+					onClick={scrollRight}
+				>
+					<FaCircleChevronRight />
+				</div>
 			</div>
 		</div>
 	);
